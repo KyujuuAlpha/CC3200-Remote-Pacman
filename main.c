d51ca6583f3f6627a0dfa9c60995a13189f57ffc
KyujuuAlpha <troiryan@att.net>
2020-03-10 23:03:21 -0700

    some reorganization

--- main.c
+++ main.c
@@ -42,6 +42,10 @@
 #define PAC_SIZE         4
 #define MAX_VEL          1
 
+#define START_STATE 0
+#define GAME_STATE  1
+#define GOVER_STATE 2
+
 #if defined(ccs)
 extern void (* const g_pfnVectors[])(void);
 #endif
@@ -71,8 +75,12 @@ static char *encodeCoords(int *coords, int size);
 static char *integerToString(int i);
 static char *coordsToString(int i, int j);
 static unsigned long getCurrentSysTimeMS(void);
-static void gameInit(void);
 static void BoardInit(void);
+
+static void mainGameLogic(void);
+static void gameOverLogic(void);
+static void startScreenLogic(void);
+static void gameLoop(void);
 
 // main function definition
 void main() {
@@ -115,7 +123,7 @@ void main() {
 
     // Initialize adafruit, then call the game loop
     Adafruit_Init();
-    while(1) gameInit();
+    gameLoop();
 }
 
 // Board initialization function
@@ -141,12 +149,130 @@ static void BoardInit(void) {
     PRCMCC3200MCUInit();
 }
 
-// Function definitions
+// some function definitions
 int abs(int val) {
     if (val < 0) return -val;
     return val;
 }
 
+static char translation[32] = {'0', '1', '2', '3', '4', '5', '6',
+                               '7', '8', '9', 'a', 'b', 'c', 'd',
+                               'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                               'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                               's', 't', 'u', 'v'};
+
+static char *encodeCoords(int *coords, int size) {
+    static char encode[] = "";
+    int i;
+    for (i = 0; i < size; i++) {
+        sprintf(encode, "%s%c", encode, translation[coords[i]]);
+    }
+    return encode;
+}
+
+static char *integerToString(int i) {
+    static char stringBuf[] = "";
+    sprintf(stringBuf, "%d", i);
+    return stringBuf;
+}
+
+static char *coordsToString(int i, int j) {
+    static char stringBuf[] = "";
+    sprintf(stringBuf, "%d %d", i, j);
+    return stringBuf;
+}
+
+
+// MAIN GAME LOOP STUFF
+static int frameDrop = 0;
+static long dropFrame(long frameCount) {
+    frameDrop++;
+    return (frameCount += 888800) < 0 ? dropFrame(frameCount) : frameCount;
+}
+
+static unsigned long getCurrentSysTimeMS(void) {
+    return (unsigned long) ((unsigned long long) PRCMSlowClkCtrGet() / 32768.0 * 1000.0);
+}
+
+static char state;
+static void gameLoop(void) {
+    // main game loop
+    unsigned long prevTime = getCurrentSysTimeMS();
+    long newDelay = 0;
+    state = START_STATE; // initial state
+
+    while (1) {
+        do {
+            switch (state) {
+                case START_STATE:
+                    startScreenLogic();
+                    break;
+                case GAME_STATE:
+                    mainGameLogic();
+                    break;
+                case GOVER_STATE:
+                    gameOverLogic();
+                    break;
+            }
+
+        } while (frameDrop-- > 0);
+        updateSoundModules();
+
+        // timing handling
+        newDelay = (long)(33.33 - (((long) getCurrentSysTimeMS() - (long) prevTime)) * 26666.67);
+        frameDrop = 0;
+        if (newDelay < 0) { // dropped a frame
+            newDelay = dropFrame(newDelay);
+        }
+        MAP_UtilsDelay((unsigned long) newDelay);
+        prevTime = getCurrentSysTimeMS();
+    }
+}
+
+// INITIAL STATE STUFF
+const int blockSize = WIDTH / MAP_SIZE;
+
+static struct Pac pac; // structure that keeps track of the pac's loc
+static struct Baddie badGuys[4] = {
+                             { 0, 0, BAD_1_COLOR },
+                             { 0, 0, BAD_2_COLOR },
+                             { 0, 0, BAD_3_COLOR },
+                             { 0, 0, BAD_4_COLOR }
+                            };
+static int xVel = 0, yVel = 0; // velocities of the pac
+static int tickTimer = 0, tickCounter = 0;
+
+static void startScreenLogic(void) {
+    fillScreen(0x0000); // first clear the screen
+    // intial stuff
+    int i, j, k, initBaddie = 0;
+    for (i = 0; i < MAP_SIZE; i++) {
+        for (j = 0; j < MAP_SIZE; j++) {
+            if (map[j][i] == WALL) {
+                fillRect(i * blockSize, j * blockSize, blockSize, blockSize, WALL_COLOR);
+            } else if (map[j][i] == POINT) { // point pac
+                k =  blockSize / 2;
+                fillRect(i * blockSize + blockSize / 2 - k / 2, j * blockSize + blockSize / 2 - k / 2, k, k, POINT_COLOR);
+            } else if (map[j][i] == SPAWN) { // start loc player
+                pac.y = j*4;
+                pac.x = i*4;
+                pac.score = 0;
+                drawScore(pac);
+            } if (map[j][i] == 4) { // start loc baddies
+                if (initBaddie >= 4) continue;
+                badGuys[initBaddie].y = j*4;
+                badGuys[initBaddie].x = i*4;
+                badGuys[initBaddie].dir = 'L';
+                initBaddie++;
+            }
+        }
+    }
+    tickTimer = 0;
+    tickCounter = 0;
+    state = GAME_STATE; // switch to main game state, there is a possibility for a title screen
+}
+
+// MAIN GAME STUFF
 static bool yCollision(int x, int y, int yVel) {
     // Wall check
     const int blockSize = WIDTH / MAP_SIZE;
@@ -173,16 +299,6 @@ static bool xCollision(int x, int y, int xVel) {
         blockX = (x + xVel + PAC_SIZE - 1) / blockSize;
     }
     return map[blockY][blockX] == 1;
-}
-
-void gameOver(struct Pac* pac) {
-    fillRect(0, 0, WIDTH, HEIGHT, 0x0000);
-    setCursor(WIDTH / 2 - 32, HEIGHT / 2 - 16);
-    Outstr("GAME OVER");
-    setCursor(WIDTH / 2 - 32, HEIGHT / 2 - 8);
-    Outstr("Score: ");
-    Outstr(integerToString(pac->score));
-    delay(100);
 }
 
 bool enemyHit(struct Pac* pac, struct Baddie* bad) {
@@ -263,156 +379,81 @@ static int adjustVel(int vel, const int *velFactor) {
     return -(vel * (*velFactor) / (255 / 2)); // adjust the velocity accordingly
 }
 
-static int frameDrop = 0;
-
-static long dropFrame(long frameCount) {
-    frameDrop++;
-    return (frameCount += 888800) < 0 ? dropFrame(frameCount) : frameCount;
-}
-
-static char translation[32] = {'0', '1', '2', '3', '4', '5', '6',
-                               '7', '8', '9', 'a', 'b', 'c', 'd',
-                               'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                               'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                               's', 't', 'u', 'v'};
-
-static char *encodeCoords(int *coords, int size) {
-    static char encode[] = "";
-    int i;
-    for (i = 0; i < size; i++) {
-        sprintf(encode, "%s%c", encode, translation[coords[i]]);
-    }
-    return encode;
-}
-
-static char *integerToString(int i) {
-    static char stringBuf[] = "";
-    sprintf(stringBuf, "%d", i);
-    return stringBuf;
-}
-
-static char *coordsToString(int i, int j) {
-    static char stringBuf[] = "";
-    sprintf(stringBuf, "%d %d", i, j);
-    return stringBuf;
-}
-
-static unsigned long getCurrentSysTimeMS(void) {
-    return (unsigned long) ((unsigned long long) PRCMSlowClkCtrGet() / 32768.0 * 1000.0);
-}
-
-void drawScore(struct Pac pac) {
+static void drawScore(struct Pac pac) {
     fillRect(12, 4, 16, 8, 0x0000);
     setCursor(12,4);
     Outstr(integerToString(pac.score));
 }
 
-static void gameInit(void) {
-    struct Pac pac; // structure that keeps track of the pac's loc
-    struct Baddie badGuys[4] = {
-                                 { 0, 0, BAD_1_COLOR },
-                                 { 0, 0, BAD_2_COLOR },
-                                 { 0, 0, BAD_3_COLOR },
-                                 { 0, 0, BAD_4_COLOR }
-                                };
-    unsigned char ACCDEV = 0x18, xREG = 0x3, yREG = 0x5; // device and registers for accel
-    const int velFactor = 15; // max velocity
+static unsigned char ACCDEV = 0x18, xREG = 0x3, yREG = 0x5; // device and registers for accel
+static const int velFactor = 15; // max velocity;
 
-    unsigned char dataBuf; // buffer that holds what was returned from a register
-    unsigned int color = PLAYER_COLOR; // starting color of the pac
-    int xVel = 0; // velocities of the pac
-    int yVel = 0;
-    int initBaddie = 0;
-    
-    fillScreen(0x0000); // first clear the screen
+// this is called every 33 ms, barring the that frames are skipped!
+static void mainGameLogic(void) {
+    if (tickTimer >= 2) { // get new data 10 times a second
+        unsigned char dataBuf; // buffer that holds what was returned from a register
+        tickTimer = 0;
+        I2C_IF_Write(ACCDEV, &xREG, 1, 0); // get the x and y accelerometer information using i2c
+        I2C_IF_Read(ACCDEV, &dataBuf, 1);  // and adjust the velocities accordingly.
+        yVel = adjustVel((int) dataBuf, &velFactor);
+        I2C_IF_Write(ACCDEV, &yREG, 1, 0);  // the x and y values from the registers are flipped
+        I2C_IF_Read(ACCDEV, &dataBuf, 1);   // since we found that they changed the wrong axis
+        xVel = adjustVel((int) dataBuf, &velFactor);
 
-    // intial stuff
-    const int blockSize = WIDTH / MAP_SIZE;
-    int i, j, k;
-    for (i = 0; i < MAP_SIZE; i++) {
-        for (j = 0; j < MAP_SIZE; j++) {
-            if (map[j][i] == WALL) {
-                fillRect(i * blockSize, j * blockSize, blockSize, blockSize, WALL_COLOR);
-            } else if (map[j][i] == POINT) { // point pac
-                k =  blockSize / 2;
-                fillRect(i * blockSize + blockSize / 2 - k / 2, j * blockSize + blockSize / 2 - k / 2, k, k, POINT_COLOR);
-            } else if (map[j][i] == SPAWN) { // start loc player
-                pac.y = j*4;
-                pac.x = i*4;
-                pac.score = 0;
-                drawScore(pac);
-            } if (map[j][i] == 4) { // start loc baddies
-                if (initBaddie >= 4) continue;
-                badGuys[initBaddie].y = j*4;
-                badGuys[initBaddie].x = i*4;
-                badGuys[initBaddie].dir = 'L';
-                initBaddie++;
-            }
+        if (tickCounter > 100) { // send a get request every 10 seconds to check for new baddies
+            playSound(BEEP);
+            tickCounter = 0;
+            buildRequest("pac_loc", coordsToString(pac.x, pac.y));
+            buildRequest("b1_loc", coordsToString(badGuys[0].x, badGuys[0].y));
+            buildRequest("b2_loc", coordsToString(badGuys[1].x, badGuys[1].y));
+            buildRequest("b3_loc", coordsToString(badGuys[2].x, badGuys[2].y));
+            buildRequest("b4_loc", coordsToString(badGuys[3].x, badGuys[3].y));
+            sendRequest();
+
+            receiveString();
+        } else {
+            tickCounter++;
+        }
+    } else {
+        tickTimer++;
+    }
+    if (xVel != 0 || yVel != 0) {
+        fillRect(pac.x, pac.y, PAC_SIZE, PAC_SIZE, 0x0000);  // erase the old location of the pac
+    }
+    updatePacLoc(&pac, &xVel, &yVel); // update the pac's location
+    fillRect(pac.x, pac.y, PAC_SIZE, PAC_SIZE, PLAYER_COLOR); // draw new ball on the screen
+    int bad;
+    for (bad = 0; bad < 4; bad++) {
+        fillRect(badGuys[bad].x, badGuys[bad].y, PAC_SIZE, PAC_SIZE, 0x0000);
+        updateBaddieLoc(&badGuys[bad]);
+        fillRect(badGuys[bad].x, badGuys[bad].y, PAC_SIZE, PAC_SIZE, badGuys[bad].color);
+        if(enemyHit(&pac, &badGuys[bad])) {
+            tickTimer = 0;
+            state = GOVER_STATE;
+            return;
         }
     }
+    if(map[pac.y/blockSize][pac.x/blockSize] == 2) {
+        map[pac.y/blockSize][pac.x/blockSize] = 0;
+        pac.score++;
+        drawScore(pac);
+    }
+}
 
-    // game loop
-    unsigned long prevTime = getCurrentSysTimeMS();
-    long newDelay = 0;
-    unsigned char tickTimer = 0, tickCounter = 0;
-    while (1) {
-        do {
-            if (tickTimer >= 2) { // get new data 10 times a second
-                tickTimer = 0;
-                I2C_IF_Write(ACCDEV, &xREG, 1, 0); // get the x and y accelerometer information using i2c
-                I2C_IF_Read(ACCDEV, &dataBuf, 1);  // and adjust the velocities accordingly.
-                yVel = adjustVel((int) dataBuf, &velFactor);
-                I2C_IF_Write(ACCDEV, &yREG, 1, 0);  // the x and y values from the registers are flipped
-                I2C_IF_Read(ACCDEV, &dataBuf, 1);   // since we found that they changed the wrong axis
-                xVel = adjustVel((int) dataBuf, &velFactor);
-
-                if (tickCounter > 100) { // send a get request every 10 seconds to check for new baddies
-                    playSound(DEATH);
-                    tickCounter = 0;
-                    buildRequest("pac_loc", coordsToString(pac.x, pac.y));
-                    buildRequest("b1_loc", coordsToString(badGuys[0].x, badGuys[0].y));
-                    buildRequest("b2_loc", coordsToString(badGuys[1].x, badGuys[1].y));
-                    buildRequest("b3_loc", coordsToString(badGuys[2].x, badGuys[2].y));
-                    buildRequest("b4_loc", coordsToString(badGuys[3].x, badGuys[3].y));
-                    sendRequest();
-
-                    receiveString();
-                } else {
-                    tickCounter++;
-                }
-            } else {
-                tickTimer++;
-            }
-            if (xVel != 0 || yVel != 0) {
-                fillRect(pac.x, pac.y, PAC_SIZE, PAC_SIZE, 0x0000);  // erase the old location of the pac
-            }
-            updatePacLoc(&pac, &xVel, &yVel); // update the pac's location
-            fillRect(pac.x, pac.y, PAC_SIZE, PAC_SIZE, color); // draw new ball on the screen
-            int bad;
-            for (bad = 0; bad < 4; bad++) {
-                fillRect(badGuys[bad].x, badGuys[bad].y, PAC_SIZE, PAC_SIZE, 0x0000);
-                updateBaddieLoc(&badGuys[bad]);
-                fillRect(badGuys[bad].x, badGuys[bad].y, PAC_SIZE, PAC_SIZE, badGuys[bad].color);
-                if(enemyHit(&pac, &badGuys[bad])) {
-                    gameOver(&pac);
-                    return;
-                }
-            }
-            if(map[pac.y/blockSize][pac.x/blockSize] == 2) {
-                map[pac.y/blockSize][pac.x/blockSize] = 0;
-                pac.score++;
-                drawScore(pac);
-            }
-        } while (frameDrop-- > 0);
-        updateSoundModules();
-
-        // timing handling
-        newDelay = (long)(33.33 - (((long) getCurrentSysTimeMS() - (long) prevTime)) * 26666.67);
-        frameDrop = 0;
-        if (newDelay < 0) { // dropped a frame
-            newDelay = dropFrame(newDelay);
-        }
-        MAP_UtilsDelay((unsigned long) newDelay);
-        prevTime = getCurrentSysTimeMS();
+// GAME OVER STUFF
+static void gameOverLogic(void) {
+    if (tickTimer == 0) {
+        fillRect(0, 0, WIDTH, HEIGHT, 0x0000);
+        setCursor(WIDTH / 2 - 32, HEIGHT / 2 - 16);
+        Outstr("GAME OVER");
+        setCursor(WIDTH / 2 - 32, HEIGHT / 2 - 8);
+        Outstr("Score: ");
+        Outstr(integerToString(pac.score));
+        playSound(DEATH);
+    }
+    if (tickTimer > 30 * 5) { // wait five seconds
+        state = START_STATE;
+    } else {
+        tickTimer++;
     }
 }
