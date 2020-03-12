4f1dcfbe3dbfeff1dcfb8d5e495254b86fa76589
FluffyThunder <corbin.ebh@gmail.com>
2020-03-12 12:23:41 -0700

    comments

--- main.c
+++ main.c
@@ -57,7 +57,7 @@ extern void (* const g_pfnVectors[])(void);
 extern uVectorEntry __vector_table;
 #endif
 
-// ball struct
+// pac struct
 struct Pac {
     int x;
     int y;
@@ -76,20 +76,20 @@ struct Baddie {
     bool validMoves[4];
 };
 // static function prototypes
-static void updatePacLoc(struct Pac *Pac, int *xVel, int *yVel);
-static int adjustVel(int vel, const int *velFactor);
-static long dropFrame(long frameCount);
+static void updatePacLoc(struct Pac *Pac, int *xVel, int *yVel); // updates pac loc based on vel
+static int adjustVel(int vel, const int *velFactor); // adjusts raw readout from accelerometer to usable vel
+static long dropFrame(long frameCount); // tracks number of dropped frames
 static char *integerToString(int i);
 static char *coordsToString(int i, int j);
 static unsigned long getCurrentSysTimeMS(void);
-void decideVelocities(struct Baddie *bad);
-static void determineValidMoves(struct Baddie *bad);
+void decideVelocities(struct Baddie *bad); // decides next move direction of bad based on queue & valid moves
+static void determineValidMoves(struct Baddie *bad); // sets valid moves of bads
 static void BoardInit(void);
 
-static void mainGameLogic(void);
-static void gameOverLogic(void);
-static void startScreenLogic(void);
-static void gameLoop(void);
+static void mainGameLogic(void); // game logic for playing
+static void gameOverLogic(void); // logic for reseting
+static void startScreenLogic(void); // logic for init
+static void gameLoop(void); // loop over logics based on state
 
 // main function definition
 void main() {
@@ -161,6 +161,7 @@ static void BoardInit(void) {
 }
 
 // some function definitions
+
 int abs(int val) {
     if (val < 0) return -val;
     return val;
@@ -168,13 +169,13 @@ int abs(int val) {
 
 static char *integerToString(int i) {
     static char stringBufA[20] = "";
-    sprintf(stringBufA, "%d", i);
+    sprintf(stringBufA, "%d", i); // prints int to buffer
     return stringBufA;
 }
 
 static char *coordsToString(int i, int j) {
     static char stringBufB[20] = "";
-    sprintf(stringBufB, "%d %d", i, j);
+    sprintf(stringBufB, "%d %d", i, j); // prints coords to buff
     return stringBufB;
 }
 
@@ -187,6 +188,8 @@ static long dropFrame(long delay) {
 }
 
 static unsigned long getCurrentSysTimeMS(void) {
+    // slow clock keeps track of real time, used to establish connection and seed random
+    // convert PRCMSlowClkCtrGet ticks to time
     return (unsigned long) ((unsigned long long) PRCMSlowClkCtrGet() / 32768.0 * 1000.0);
 }
 
@@ -208,17 +211,17 @@ static void gameLoop(void) {
         do {
             switch (state) {
                 case START_STATE:
-                    startScreenLogic();
+                    startScreenLogic(); // updates state to game_state
                     break;
-                case GAME_STATE:
+                case GAME_STATE: //runs until game over reached
                     mainGameLogic();
                     break;
-                case GOVER_STATE:
+                case GOVER_STATE: // resets state to start after 5 seconds
                     gameOverLogic();
                     break;
             }
 
-        } while (frameDrop-- > 0);
+        } while (frameDrop-- > 0); // once a certain number of frames drop, reset frames
         updateSoundModules();
 
         // timing handling
@@ -246,42 +249,47 @@ static int xVel = 0, yVel = 0; // velocities of the pac
 static int tickTimer = 0, tickCounter = 0, selectedBaddie = -1;
 
 static void drawScore(void) {
-    fillRect(12, 4, 17, 8, 0x0000);
+    fillRect(12, 4, 17, 8, 0x0000); // clears current written score
     setCursor(12,4);
-    Outstr(integerToString(pac.score));
+    Outstr(integerToString(pac.score)); // prints score in upper right
 }
 
 static void startScreenLogic(void) {
     fillScreen(0x0000); // first clear the screen
     int i, j, k, initBaddie = 0;
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++) { // init all bads to impossible location so program knows they aren't initialized
         badGuys[i].x = -1;
         badGuys[i].y = -1;
     }
-    // intial stuff
+    // initial stuff
+    // iterate through map, setting info based on values in each entry
     for (i = 0; i < MAP_SIZE; i++) {
         for (j = 0; j < MAP_SIZE; j++) {
             if (map[j][i] == WALL) {
+                // draw wall tile
                 fillRect(i * blockSize, j * blockSize, blockSize, blockSize, WALL_COLOR);
             } else if (map[j][i] == POINT || map[j][i] == PLACEHOLDER) { // point pac
+                // draw point pellet, reset inactive pellets to active
                 map[j][i] = POINT;
-                k =  blockSize / 2;
+                k =  blockSize / 2; // middle of tile to draw pellet
                 fillRect(i * blockSize + blockSize / 2 - k / 2, j * blockSize + blockSize / 2 - k / 2, k, k, POINT_COLOR);
             } else if (map[j][i] == SPAWN) { // start loc player
+                // set pac location
                 pac.y = j*4;
                 pac.x = i*4;
                 pac.score = 0;
                 drawScore();
             } else if (map[j][i] == ENEMY) { // start loc baddies
-                if (initBaddie >= 4) continue;
+                // init next bad
+                if (initBaddie >= 4) continue; // do not init more than 4 bads
                 badGuys[initBaddie].y = j*4;
                 badGuys[initBaddie].x = i*4;
                 badGuys[initBaddie].velY = 0;
                 badGuys[initBaddie].velX = 0;
-                strcpy(badGuys[initBaddie].dirQueue, "");
+                strcpy(badGuys[initBaddie].dirQueue, ""); // clear dir queue
                 badGuys[initBaddie].ready = false;
                 determineValidMoves(&badGuys[initBaddie]);
-                decideVelocities(&badGuys[initBaddie]);
+                decideVelocities(&badGuys[initBaddie]); // set random move dir
                 initBaddie++;
             }
         }
@@ -296,37 +304,38 @@ static void startScreenLogic(void) {
 // MAIN GAME STUFF
 static bool yCollision(int x, int y, int yVel) {
     // Wall check
-    const int blockSize = WIDTH / MAP_SIZE;
+    const int blockSize = WIDTH / MAP_SIZE; // set pix size of grid tiles
     int blockX, blockY;
-    blockX = (x + 2) / blockSize;
+    blockX = (x + 2) / blockSize; // the X coor of grid tile that mid pixel of top/bottom edge lies in
     if (yVel < 0) {
-        blockY = (y + yVel) / blockSize;
+        blockY = (y + yVel) / blockSize; // Y coor of grid tile that mid pixel of top edge lies
     } else {
-        blockY = (y + yVel + PAC_SIZE - 1) / blockSize;
+        blockY = (y + yVel + PAC_SIZE - 1) / blockSize; // Y coor of grid tile that mid pixel of bottom edge lies
     }
-    return map[blockY][blockX] == WALL;
+    return map[blockY][blockX] == WALL; // check map tile that enemy would move into
 }
 
 static bool xCollision(int x, int y, int xVel) {
     // Wall check
-    const int blockSize = WIDTH / MAP_SIZE;
+    const int blockSize = WIDTH / MAP_SIZE; // set pix size of grid tiles
     int blockX, blockY;
-    blockY = (y+2) / blockSize;
+    blockY = (y+2) / blockSize; // the Y coor of grid tile that mid pixel of left/right edge lies in
     if (xVel < 0) {
-        blockX = (x + xVel) / blockSize;
+        blockX = (x + xVel) / blockSize; // X coor of grid tile that mid pixel of left edge lies
     } else {
-        blockX = (x + xVel + PAC_SIZE - 1) / blockSize;
+        blockX = (x + xVel + PAC_SIZE - 1) / blockSize; // X coor of grid tile that mid pixel of right edge lies
     }
-    return map[blockY][blockX] == WALL;
+    return map[blockY][blockX] == WALL; // check map tile that enemy would move into
 }
 
 bool enemyHit(struct Pac* pac, struct Baddie* bad) {
+    // check if enemy and pac share grid coords
     return ((int)pac->x/4 == (int)bad->x/4 && (int)pac->y/4 == (int)bad->y/4);
 }
 
 void decideVelocities(struct Baddie *bad) {
     char dirChoice = 4, i;
-    if (bad->dirQueue[0] != '\0') {
+    if (bad->dirQueue[0] != '\0') { // dir queue not empty
         do {
             if (bad->dirQueue[0] == '\0') {
                 break;
@@ -338,40 +347,40 @@ void decideVelocities(struct Baddie *bad) {
                 i++;
             }
             if (bad->dirQueue[0] == '\0') {
-                bad->ready = true;
+                bad->ready = true; // set enemy ready to recieve next dir
             }
-        } while (!bad->validMoves[dirChoice]);
-    } else if(bad->id != selectedBaddie) {
+        } while (!bad->validMoves[dirChoice]); // find first valid move in queue, set that as dir
+    } else if(bad->id != selectedBaddie) { // queue empty
         srand((unsigned int) getCurrentSysTimeMS());
         int sanityCheck = 0; // prevents looping forever in case no valid moves
-        dirChoice = rand() % 4;
+        dirChoice = rand() % 4; // choose ran num 0-3 to start in valid moves
         while (!bad->validMoves[dirChoice] && sanityCheck < 4) {
-            dirChoice++;
+            dirChoice++; // iterate through dir till valid found
             sanityCheck++;
             if (dirChoice == 4) dirChoice = 0;
         }
     }
     switch (dirChoice) {
         case 0:
-            bad->velY = -1;
+            bad->velY = -1; // move up
             break;
         case 1:
-            bad->velX = -1;
+            bad->velX = -1; // move left
             break;
         case 2:
-            bad->velX = 1;
+            bad->velX = 1; // move down
             break;
         case 3:
-            bad->velY = 1;
+            bad->velY = 1; // move right
             break;
     }
 }
 
 static void updateBaddieLoc(struct Baddie* bad) {
     if(bad->velY != 0) {
-        if (!yCollision(bad->x, bad->y, bad->velY)) {
+        if (!yCollision(bad->x, bad->y, bad->velY)) { // if there's no collision, move
             bad->y += bad->velY;
-            bad->x = ((bad->x + 2) / 4) * 4;
+            bad->x = ((bad->x + 2) / 4) * 4; // fix pac x to left side of grid tile
         } else {
             bad->velY = 0;
         }
@@ -379,23 +388,23 @@ static void updateBaddieLoc(struct Baddie* bad) {
     if(bad->velX != 0) {
         if (!xCollision(bad->x, bad->y, bad->velX)) {
             bad->x += bad->velX;
-            bad->y = ((bad->y + 2) / 4) * 4;
+            bad->y = ((bad->y + 2) / 4) * 4; // fix pac y to top side of grid tile
         } else {
             bad->velX = 0;
         }
     }
-    determineValidMoves(bad);
+    determineValidMoves(bad); // update valid moves based on new position
 }
 
 // function that updates the Pac's location accordingly and keeps it within bounds
 static void updatePacLoc(struct Pac *pac, int *xVel, int *yVel) {
 
-    bool yGreater = abs(*yVel) > abs(*xVel);
-    if(*yVel > MAX_VEL) *yVel = MAX_VEL;
+    bool yGreater = abs(*yVel) > abs(*xVel); // determine greater vel
+    if(*yVel > MAX_VEL) *yVel = MAX_VEL; // bind vel to +-1
     if(*yVel < -MAX_VEL) *yVel = -MAX_VEL;
     if(*xVel > MAX_VEL) *xVel = MAX_VEL;
     if(*xVel < -MAX_VEL) *xVel = -MAX_VEL;
-    if (*yVel == 0 && *xVel == 0) return;
+    if (*yVel == 0 && *xVel == 0) return; // no movement
 
     if(yCollision(pac->x, pac->y, *yVel) && xCollision(pac->x, pac->y, *xVel)) { // there's a collision in both directions
         *xVel = 0;
@@ -417,6 +426,7 @@ static void updatePacLoc(struct Pac *pac, int *xVel, int *yVel) {
             (*pac).x += *xVel;
             (*pac).y = (((*pac).y + 2) / 4) * 4;
             return;
+            // move x, position y on rails
         }
         if (!yCollision(pac->x, pac->y, *yVel)) { // no collision moving in y
             (*pac).y += *yVel;
@@ -439,10 +449,10 @@ static int adjustVel(int vel, const int *velFactor) {
 static void parseGETRequest(char *request) {
     parseJSON(request);
     char *val;
-    if (badGuys[0].dirQueue[0] == '\0' && !badGuys[0].ready) {
-        val = getValue("b1_q");
-        if (strcmp(val, "ready") != 0) {
-            strcpy(badGuys[0].dirQueue, val);
+    if (badGuys[0].dirQueue[0] == '\0' && !badGuys[0].ready) { // badGuy is ready to recieve new commands
+        val = getValue("b1_q"); // retireve shadow update
+        if (strcmp(val, "ready") != 0) { // there was something to recieve
+            strcpy(badGuys[0].dirQueue, val); // copy vals to queue
             selectedBaddie = 0;
         }
     }
@@ -487,7 +497,7 @@ static void mainGameLogic(void) {
         I2C_IF_Read(ACCDEV, &dataBuf, 1);   // since we found that they changed the wrong axis
         xVel = adjustVel((int) dataBuf, &velFactor);
 
-        if (pollReceiveMode) {
+        if (pollReceiveMode) { // retireve shadow update
             receive = networkReceive();
             if (strlen(receive) > 4) {
                 if (!requestFlag) {
@@ -550,35 +560,40 @@ static void mainGameLogic(void) {
     fillRect(pac.x, pac.y, PAC_SIZE, PAC_SIZE, PLAYER_COLOR); // draw new pac on the screen
     int bad;
     pointIndex = 0;
-    for (bad = 0; bad < 4; bad++) {
+    for (bad = 0; bad < 4; bad++) { // iterate through bads
+        // get grid coord of cur bad
         int badGridX = badGuys[bad].x/4;
         int badGridY = badGuys[bad].y/4;
-        fillRect(badGuys[bad].x, badGuys[bad].y, PAC_SIZE, PAC_SIZE, 0x0000);
+        fillRect(badGuys[bad].x, badGuys[bad].y, PAC_SIZE, PAC_SIZE, 0x0000); // clear bad from screen
+        // Check the grid locs around bad and set them to fill them with point pellet to prevent enemy from erasing them
         if (map[badGridY][badGridX] == POINT) {
             pointX[pointIndex] = badGridX;
             pointY[pointIndex++] = badGridY;
         }
+        // check position below
         if ((badGuys[bad].y + (blockSize / 2)) / 4 != badGridY && map[badGridY + 1][badGridX] == POINT) {
             pointX[pointIndex] = badGridX;
             pointY[pointIndex++] = badGridY + 1;
         }
+        // check position to right
         if ((badGuys[bad].x + (blockSize / 2)) / 4 != badGridX && map[badGridY][badGridX + 1] == POINT) {
             pointX[pointIndex] = badGridX + 1;
             pointY[pointIndex++] = badGridY;
         }
-        updateBaddieLoc(&badGuys[bad]);
-        // if velocities zero
+        updateBaddieLoc(&badGuys[bad]); // try to move bad
+        // if velocities zero (collision)
         if (badGuys[bad].velX == 0 && badGuys[bad].velY == 0) {
-            decideVelocities(&badGuys[bad]);
+            decideVelocities(&badGuys[bad]); // next move in queue or random valid
         }
-        fillRect(badGuys[bad].x, badGuys[bad].y, PAC_SIZE, PAC_SIZE, badGuys[bad].color);
-        if(enemyHit(&pac, &badGuys[bad])) {
+        fillRect(badGuys[bad].x, badGuys[bad].y, PAC_SIZE, PAC_SIZE, badGuys[bad].color); // redraw enemy in ne loc
+        if(enemyHit(&pac, &badGuys[bad])) { // check if enemy collision with pac
             tickTimer = 0;
-            state = GOVER_STATE;
+            state = GOVER_STATE; // gg u loose
             return;
         }
     }
     int i, k = blockSize / 2;
+    // fill all point tiles that were erased by bads
     for (i = 0; i < pointIndex; i++) {
         for (bad = 0; bad < 4; bad++) {
             if (pointX[i] == badGuys[bad].x/4 && pointY[i] == badGuys[bad].y/4) {
@@ -591,6 +606,7 @@ static void mainGameLogic(void) {
     }
 
     if(map[pac.y/blockSize][pac.x/blockSize] == POINT) {
+        // update score if pac has entered a point tile
         map[pac.y/blockSize][pac.x/blockSize] = PLACEHOLDER;
         pac.score++;
         drawScore();
@@ -601,11 +617,13 @@ static void mainGameLogic(void) {
 // GAME OVER STUFF
 static void gameOverLogic(void) {
     if (tickTimer == 0) {
+        // clear screen
         fillRect(0, 0, WIDTH, HEIGHT, 0x0000);
         setCursor(WIDTH / 2 - 32, HEIGHT / 2 - 16);
         Outstr("GAME OVER");
         setCursor(WIDTH / 2 - 32, HEIGHT / 2 - 8);
         Outstr("Score: ");
+        // draw final score
         Outstr(integerToString(pac.score));
         playSound(DEATH);
     }
